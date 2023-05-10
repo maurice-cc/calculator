695dede5e42ba99923253a3b908f9cc50ac06d6b
maurice-cc <129709579+maurice-cc@users.noreply.github.com>
2023-05-10 17:08:08 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const [displayValue, setDisplayValue] = useState("0");
-  const [typeInValue, setTypeInValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
   const [pendingOperation, setPendingOperation] = useState(null);
   const [storedValue, setStoredValue] = useState(0);
   const [isOperationOn, setIsOperationOn] = useState(false);
@@ -13,27 +13,36 @@ function App() {
     pendingOperation,
     storedValue,
     isOperationOn,
-    typeInValue
+    isValid,
+    /^([^.\n]*)(\.[^.\n]*)?$/.test(displayValue)
   );
 
-  const onDigitClick = (digit) => {
+  const digitCheck = (value) => {
+    const pattern = /^([^.\n]*)(\.[^.\n]*)?$/;
+    return pattern.test(value);
+  };
+
+  const handleDigitClick = (digit) => {
+    if (!digitCheck(displayValue + digit)) {
+      return;
+    }
     if (displayValue === "0") {
-      setDisplayValue("");
+      setDisplayValue(digit === "." ? "0" : "");
       setDisplayValue((displayValue) => displayValue + String(digit));
     } else if (isOperationOn) {
       setDisplayValue("");
       setDisplayValue((displayValue) => displayValue + String(digit));
+      setIsValid(true);
       setIsOperationOn(false);
     } else {
       setDisplayValue((displayValue) => displayValue + String(digit));
     }
-    //setTypeInValue();
   };
 
-  const onOperatorClick = (operator) => {
+  const handleOperatorClick = (operator) => {
     if (storedValue === 0) {
       setStoredValue(parseFloat(displayValue));
-    } else if (pendingOperation && operator === "=") {
+    } else if (pendingOperation && isValid && operator === "=") {
       const newValue = performOperation(
         storedValue,
         parseFloat(displayValue),
@@ -41,6 +50,7 @@ function App() {
       );
       setDisplayValue(String(newValue));
       setStoredValue(newValue);
+      setIsValid(false);
     }
     if (operator == "+/-" || operator == "%") {
       const newValue = performOperation(0, parseFloat(displayValue), operator);
@@ -70,11 +80,25 @@ function App() {
     }
   };
 
+  const handleClick = (value) => {
+    switch (typeof value) {
+      case "number":
+        handleDigitClick(value);
+        break;
+      case "string":
+        handleOperatorClick(value);
+        break;
+      default:
+        break;
+    }
+  };
+
   const onClearClick = () => {
     setDisplayValue("0");
     setPendingOperation(null);
     setStoredValue(0);
     setIsOperationOn(false);
+    setIsValid(false);
   };
 
   return (
@@ -82,27 +106,41 @@ function App() {
       <div className="calculator">
         <div className="display">{displayValue}</div>
         <div className="button-grid">
-          <button onClick={onClearClick}>C</button>
-          <button onClick={() => onOperatorClick("+/-")}>+/-</button>
-          <button onClick={() => onOperatorClick("%")}>%</button>
-          <button onClick={() => onOperatorClick("÷")}>÷</button>
-          <button onClick={() => onDigitClick(7)}>7</button>
-          <button onClick={() => onDigitClick(8)}>8</button>
-          <button onClick={() => onDigitClick(9)}>9</button>
-          <button onClick={() => onOperatorClick("×")}>×</button>
-          <button onClick={() => onDigitClick(4)}>4</button>
-          <button onClick={() => onDigitClick(5)}>5</button>
-          <button onClick={() => onDigitClick(6)}>6</button>
-          <button onClick={() => onOperatorClick("-")}>-</button>
-          <button onClick={() => onDigitClick(1)}>1</button>
-          <button onClick={() => onDigitClick(2)}>2</button>
-          <button onClick={() => onDigitClick(3)}>3</button>
-          <button onClick={() => onOperatorClick("+")}>+</button>
-          <button onClick={() => onDigitClick(0)} className="zero">
+          <button className="method" onClick={onClearClick}>
+            AC
+          </button>
+          <button className="method" onClick={() => handleClick("+/-")}>
+            +/-
+          </button>
+          <button className="method" onClick={() => handleClick("%")}>
+            %
+          </button>
+          <button className="operator" onClick={() => handleClick("÷")}>
+            ÷
+          </button>
+          <button onClick={() => handleClick(7)}>7</button>
+          <button onClick={() => handleClick(8)}>8</button>
+          <button onClick={() => handleClick(9)}>9</button>
+          <button className="operator" onClick={() => handleClick("×")}>
+            ×
+          </button>
+          <button onClick={() => handleClick(4)}>4</button>
+          <button onClick={() => handleClick(5)}>5</button>
+          <button onClick={() => handleClick(6)}>6</button>
+          <button className="operator" onClick={() => handleClick("-")}>
+            -
+          </button>
+          <button onClick={() => handleClick(1)}>1</button>
+          <button onClick={() => handleClick(2)}>2</button>
+          <button onClick={() => handleClick(3)}>3</button>
+          <button className="operator" onClick={() => handleClick("+")}>
+            +
+          </button>
+          <button className="zero" onClick={() => handleClick(0)}>
             0
           </button>
-          {/* <button onClick={() => onDigitClick(".")}>.</button> */}
-          <button onClick={() => onOperatorClick("=")} className="equals">
+          <button onClick={() => handleDigitClick(".")}>.</button>
+          <button className="equals" onClick={() => handleClick("=")}>
             =
           </button>
         </div>
